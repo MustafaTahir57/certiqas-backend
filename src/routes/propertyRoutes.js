d67ba77324b9341c84784f68bdcea14e5ac4ca19
muhammad.bilal@novatoresols.com <muhmdbilal3333@gmail.com>
2025-12-23 22:52:00 +0500

    role add

--- src/routes/propertyRoutes.js
+++ src/routes/propertyRoutes.js
@@ -10,7 +10,7 @@ const {
 const { checkRole } = require("../middlewares/roleMiddleware");
 
 const router = express.Router();
-const allowedRoles = ["SuperAdmin", "Developer"];
+const allowedRoles = ["SuperAdmin", "Developer", "Assistant"];
 router.post(
   "/create-property",
   verifyToken,
